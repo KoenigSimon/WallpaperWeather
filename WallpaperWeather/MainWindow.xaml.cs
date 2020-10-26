03177c2a63855b918f9634d869f32159446870fa
KoenigSimon <koenig.simon@t-online.de>
2020-10-26 13:13:02 +0100

    Added data class and improved bindings

--- WallpaperWeather/MainWindow.xaml.cs
+++ WallpaperWeather/MainWindow.xaml.cs
@@ -14,172 +14,37 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Runtime.CompilerServices;
 using System.ComponentModel;
+using System.Drawing.Imaging;
+using System.Collections.ObjectModel;
 
 namespace WallpaperWeather
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : INotifyPropertyChanged
+    public partial class MainWindow
     {
+        MainWindowViewModel view = new MainWindowViewModel();
         public MainWindow()
         {
+            DataContext = view;
             InitializeComponent();
-            DataContext = this;
             ImageCacheLoader.LoadImagesFromCache(Scroll);
-            city = "testingen";
-            time = "4:20";
-            temperature = "69°C";
-            timeZone = "Cool";
-            plz = "PLZ";
-        }
 
-        //enums
-        public enum enumWeather
-        {
-            CLOUDS,
-            CLOUDSRAINY,
-            THUNDERSTORM,
-            SUNNY,
-            SUNNYCLOUDS
-        }
-        public enum dayTime
-        {
-            DAWN,
-            DUSK,
-            DAY,
-            NIGHT
-        }
-        public enum season
-        {
-            SPRING,
-            SUMMER,
-            AUTUMN,
-            WINTER
-        }
 
-        //selected item Properties
-        private bool _chkBxBanUse;
-        public bool chkBxBanUse
-        {
-            get { return _chkBxBanUse; }
-            set {
-                _chkBxBanUse = value;
-                OnPropertyChanged();
-            }
-        }
-        public List<string> comboItemsWeather = new List<string>();
-        public List<string> comboItemsDayTime = new List<string>();
-        public List<string> comboItemsSeason = new List<string>();
+            view.city = "testingen";
+            view.time = "yeeettime";
 
-        //localization Properties
-        private string _temperature;
-        public string temperature
-        {
-            get { return _temperature; }
-            set {
-                _temperature = value;
-                OnPropertyChanged();
-            }
-        }
+            view.comboItemsWeather = new ObservableCollection<string>();
+            view.comboItemsWeather.Add("testitem");
+            view.comboItemsWeather.Add("testite2m");
 
-        private string _city;
-        public string city { 
-            get { return _city; }
-            set { 
-                _city = value;
-                OnPropertyChanged();
-            } 
-        }
-
-        private string _time;
-        public string time
-        {
-            get { return _time; }
-            set {
-                _time = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private Image _weatherIcon;
-        public Image weatherIcon
-        {
-            get { return _weatherIcon; }
-            set {
-                _weatherIcon = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private string _plz;
-        public string plz
-        {
-            get { return _plz; }
-            set {
-                _plz = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private string _timeZone;
-        public string timeZone
-        {
-            get { return _timeZone; }
-            set {
-                _timeZone = value;
-                OnPropertyChanged();
-            }
-        }
-
-        //option Properties
-        private bool _chkBxStartWithWindows;
-        public bool chkBxStartWithWindows
-        {
-            get { return _chkBxStartWithWindows; }
-            set {
-                _chkBxStartWithWindows = value;
-                OnPropertyChanged();
-            }
-        }
-        private bool _chkBxChangeInBackground;
-        public bool chkBxChangeInBackground
-        {
-            get { return _chkBxChangeInBackground; }
-            set {
-                _chkBxChangeInBackground = value;
-                OnPropertyChanged();
-            }
-        }
-        private bool _chkBxAutoImageSearch;
-        public bool chkBxAutoImageSearch
-        {
-            get { return _chkBxAutoImageSearch; }
-            set {
-                _chkBxAutoImageSearch = value;
-                OnPropertyChanged();
-            }
-        }
-        private string _maxImageCount;
-        public string maxImageCount
-        {
-            get { return _maxImageCount; }
-            set {
-                _maxImageCount = value;
-                OnPropertyChanged();
-            }
-        }
+        }      
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             //Passthrough to handler
             TrayHandler.MinimizeToTray(this, e);
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void ClickImportImages(object sender, RoutedEventArgs e)
@@ -194,7 +59,8 @@ namespace WallpaperWeather
 
         private void ClickExit(object sender, RoutedEventArgs e)
         {
-            Environment.Exit(1);
+            this.Hide();
+            Environment.Exit(0);
         }
 
         private void ClickDeleteImage(object sender, RoutedEventArgs e)
---
+++ WallpaperWeather/MainWindowViewModel.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Drawing;
+
+namespace WallpaperWeather
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //enums
+        public enum enumWeather
+        {
+            CLOUDS,
+            CLOUDSRAINY,
+            THUNDERSTORM,
+            SUNNY,
+            SUNNYCLOUDS
+        }
+        public enum enumdayTime
+        {
+            DAWN,
+            DUSK,
+            DAY,
+            NIGHT
+        }
+        public enum enumseason
+        {
+            SPRING,
+            SUMMER,
+            AUTUMN,
+            WINTER
+        }
+
+        #region SelectedItem Properties
+
+        private bool _chkBxBanUse;
+        public bool chkBxBanUse
+        {
+            get { return _chkBxBanUse; }
+            set
+            {
+                _chkBxBanUse = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private Image _imageSelection;
+        public Image imageSelection
+        {
+            get { return _imageSelection;  }
+            set {
+                _imageSelection = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private ObservableCollection<string> _comboItemsWeather;
+        public ObservableCollection<string> comboItemsWeather
+        {
+            get { return _comboItemsWeather; }
+            set
+            {
+                _comboItemsWeather = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private ObservableCollection<string> _comboItemsDayTime;
+        public ObservableCollection<string> comboItemsDayTime
+        {
+            get { return _comboItemsDayTime; }
+            set
+            {
+                _comboItemsDayTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private ObservableCollection<string> _comboItemsSeason;
+        public ObservableCollection<string> comboItemsSeason
+        {
+            get { return _comboItemsSeason; }
+            set
+            {
+                _comboItemsSeason = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _selectedItemWeather;
+        public string selectedItemWeather
+        {
+            get { return _selectedItemWeather; }
+            set
+            {
+                _selectedItemWeather = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _selectedItemDayTime;
+        public string selectedItemDayTime
+        {
+            get { return _selectedItemDayTime; }
+            set
+            {
+                _selectedItemDayTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _selectedItemSeason;
+        public string selectedItemSeason
+        {
+            get { return _selectedItemSeason; }
+            set
+            {
+                _selectedItemSeason = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Localization Properties
+
+        private string _temperature;
+        public string temperature
+        {
+            get { return _temperature; }
+            set
+            {
+                _temperature = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _city;
+        public string city
+        {
+            get { return _city; }
+            set
+            {
+                _city = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _time;
+        public string time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private Image _weatherIcon;
+        public Image weatherIcon
+        {
+            get { return _weatherIcon; }
+            set
+            {
+                _weatherIcon = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _plz;
+        public string plz
+        {
+            get { return _plz; }
+            set
+            {
+                _plz = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _timeZone;
+        public string timeZone
+        {
+            get { return _timeZone; }
+            set
+            {
+                _timeZone = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Option Properties
+
+        private bool _chkBxStartWithWindows;
+        public bool chkBxStartWithWindows
+        {
+            get { return _chkBxStartWithWindows; }
+            set
+            {
+                _chkBxStartWithWindows = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private bool _chkBxChangeInBackground;
+        public bool chkBxChangeInBackground
+        {
+            get { return _chkBxChangeInBackground; }
+            set
+            {
+                _chkBxChangeInBackground = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private bool _chkBxAutoImageSearch;
+        public bool chkBxAutoImageSearch
+        {
+            get { return _chkBxAutoImageSearch; }
+            set
+            {
+                _chkBxAutoImageSearch = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _maxImageCount;
+        public string maxImageCount
+        {
+            get { return _maxImageCount; }
+            set
+            {
+                _maxImageCount = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+    }
+}
