ab5db0acac84c18d4477c78a2d295575e1242099
KoenigSimon <koenig.simon@t-online.de>
2020-10-31 10:56:01 +0100

    added API Key Class

---
+++ WallpaperWeather/AuthData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallpaperWeather
+{
+    class AuthData
+    {
+        private static string apiKey = "{your API Key here}";
+
+        public static string GetOpenWeatherAPIKey()
+        {
+            return apiKey;
+        }
+    }
+}
